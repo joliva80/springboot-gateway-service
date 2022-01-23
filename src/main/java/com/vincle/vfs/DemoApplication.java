e852df4ca58413b2434f8bdbf720879b616507c3
Joan Oliva <joanoj@gmail.com>
2022-01-23 18:38:56 +0100

    simple gateway

---
+++ src/main/java/com/vincle/vfs/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.vincle.vfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
